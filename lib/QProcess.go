f54ade5d8d8ea4874cb560d4fec301b9471b93b6
Jamie <jamie@tidepool.org>
2015-11-08 23:03:47 +1300

    roll back

--- lib/QProcess.go
+++ lib/QProcess.go
@@ -5,8 +5,21 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/jh-bate/fantail-bot/Godeps/_workspace/src/github.com/tucnak/telebot"
+)
+
+const (
+	//being a good citizen
+	start_cmd, help_cmd = "/start", "/help"
+
+	chat_cmd, say_cmd, review_cmd, remind_cmd = "/chat", "/say", "/review", "/remind"
+
+	remind_cmd_hint, review_cmd_hint, say_cmd_hint = "/remind in <days> to <msg>", "/review <days>", "/say [what you want to say]"
+
+	default_script = "default"
+	stickers_chat  = "chat"
 )
 
 type (
@@ -37,18 +50,17 @@ func NewQProcess(b *telebot.Bot, s *Storage) *QProcess {
 
 func (this *QProcess) Run(input <-chan telebot.Message) {
 	for msg := range input {
-		this.s.addDetails(msg)
 
+		this.s.User = msg.Sender
 		if msg.Sticker.Exists() {
 			log.Println("incoming sticker", msg.Sticker.FileID)
 			if s := this.sLib.FindSticker(msg.Sticker.FileID); s != nil {
+				this.loadScript(stickers_chat)
 				this.
-					determineScript(msg).
 					nextStickerQ(s, msg).
 					andChat()
 			}
 		} else {
-
 			this.
 				quickWinFirst(msg).
 				determineScript(msg).
@@ -61,25 +73,26 @@ func (this *QProcess) Run(input <-chan telebot.Message) {
 
 func (this *QProcess) quickWinFirst(msg telebot.Message) *QProcess {
 
-	if this.s.Action.isHelp() {
-
-		this.s.send(fmt.Sprintf("%s %s", this.info.App, this.s.Action.typeOf))
-
-	} else if this.s.Action.setTypeMatches(say_action) && this.s.Action.hasSubmisson() {
-
-		this.s.save(NewNote(msg, this.s.Action.typeOf))
-
-	} else if this.s.Action.setTypeMatches(remind_action) && this.s.Action.hasSubmisson() {
-
-		this.s.save(NewReminderNote(msg))
-
-	} else if this.s.Action.setTypeMatches(recap_action) {
-
+	if isCmd(msg.Text, start_cmd, help_cmd) {
+		appInfo := fmt.Sprintf("%s %s %s %s ",
+			this.info.App,
+			chat_cmd+" - to have a *quick chat* about what your upto \n\n",
+			say_cmd_hint+" - to say *anything* thats on your mind \n\n",
+			review_cmd_hint+" - to review what has been happening \n\n",
+		)
+		this.s.send(appInfo)
+	} else if hasSubmisson(msg.Text, say_cmd) {
+		log.Println("save something said ", msg.Text)
+		this.s.save(NewNote(msg, say_cmd))
+	} else if hasSubmisson(msg.Text, review_cmd) || isCmd(msg.Text, review_cmd) {
+		log.Println("doing review ", msg.Text)
 		n := this.s.getNotes(msg)
 		this.s.send(this.info.Reminders...)
 		this.s.send(n.FilterBy(said_tag).ToString())
 		this.s.send(n.FilterBy(chat_tag).ToString())
-
+	} else if hasSubmisson(msg.Text, remind_cmd) {
+		log.Println("save reminder ", msg.Text)
+		this.s.save(NewReminderNote(msg))
 	}
 	return this
 }
@@ -95,20 +108,40 @@ func (this *QProcess) loadInfo() {
 	}
 }
 
+func (this *QProcess) loadScript(scriptName string) {
+	file, err := os.Open(fmt.Sprintf("./config/%s.json", scriptName))
+	if err != nil {
+		log.Panic("could not load QandA language file ", err.Error())
+	}
+	err = json.NewDecoder(file).Decode(&this.lang)
+	if err != nil {
+		log.Panic("could not decode QandA ", err.Error())
+	}
+}
+
 func (this *QProcess) determineScript(msg telebot.Message) *QProcess {
-	this.s.Action.loadQuestions(&this.lang.Questions)
+
+	if hasSubmisson(msg.Text, remind_cmd, say_cmd) {
+		log.Println("load default script after submisson")
+		this.loadScript(default_script)
+	} else if isCmd(msg.Text, chat_cmd, say_cmd, remind_cmd) {
+		words := strings.Fields(msg.Text)
+		scriptName := strings.SplitAfter(words[0], "/")[1]
+		log.Println("load command script", scriptName)
+		this.loadScript(scriptName)
+	}
 	return this
 }
 
 func (this *QProcess) nextQ(msg telebot.Message) *QProcess {
 	this.next = nil
 
-	if this.s.Action.typeIsSet() {
+	if isCmd(msg.Text, chat_cmd, say_cmd, remind_cmd) {
 		this.next = this.lang.Questions.First()
 		return this
 	} else {
 		if nxt, sv := this.lang.Questions.next(msg.Text); sv {
-			this.s.save(NewNote(msg, this.s.Action.typeOf, this.next.RelatesTo.SaveTag))
+			this.s.save(NewNote(msg, chat_cmd, this.next.RelatesTo.SaveTag))
 			this.next = nxt
 			return this
 		} else {
@@ -122,7 +155,7 @@ func (this *QProcess) nextStickerQ(s *Sticker, msg telebot.Message) *QProcess {
 	this.next = nil
 
 	if nxt, sv := this.lang.Questions.nextFrom(s.Ids...); sv {
-		this.s.save(s.ToNote(msg, this.s.Action.typeOf))
+		this.s.save(s.ToNote(msg, chat_tag))
 		this.next = nxt
 		return this
 	} else {
--- lib/Session.go
+++ lib/Session.go
@@ -19,7 +19,6 @@ type (
 		Bot     *telebot.Bot
 		User    telebot.User
 		Storage *Storage
-		Action  *action
 	}
 
 	Keyboard [][]string
@@ -27,12 +26,6 @@ type (
 
 func newSession(b *telebot.Bot, s *Storage) *session {
 	return &session{Bot: b, Storage: s}
-}
-
-func (s *session) addDetails(msg telebot.Message) {
-	s.Action = newAction(msg)
-	s.User = msg.Sender
-	return
 }
 
 func (s *session) send(msgs ...string) {
