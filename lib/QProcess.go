8c3d29b3e1b1039b91ceeaf21f864354def4dcd5
Jamie <jamie@tidepool.org>
2015-11-12 22:23:57 -0800

    debug prev name

--- lib/QProcess.go
+++ lib/QProcess.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"log"
 	"strings"
 
 	"github.com/jh-bate/fantail-bot/Godeps/_workspace/src/github.com/tucnak/telebot"
@@ -30,7 +31,9 @@ func NewQProcess(b *telebot.Bot, s *Storage) *QProcess {
 }
 
 func getActionName(msg telebot.Message) (bool, string) {
+	log.Println("Check getActionName ", msg.Text)
 	if strings.Contains(msg.Text, "/") {
+		log.Println("Check getActionName ", strings.Fields(msg.Text)[0])
 		return true, strings.Fields(msg.Text)[0]
 	}
 	return false, ""
@@ -42,10 +45,14 @@ func (this *QProcess) Run(input <-chan telebot.Message) {
 
 	for msg := range input {
 
+		log.Println("prev name", prevActionName)
+
 		in := newIncoming(msg)
 		in.getAction(this.s, prevActionName).firstUp().askQuestion()
 		if update, name := getActionName(msg); update {
 			prevActionName = name
 		}
+
+		log.Println("prev name after", prevActionName)
 	}
 }
