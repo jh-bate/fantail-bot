306d53a5539e3edd135c44f805973aaef924e497
Jamie <jamie@tidepool.org>
2015-12-07 14:07:51 +1300

    go back to old config pattern

--- lib/Config.go
+++ lib/Config.go
@@ -1,20 +1,29 @@
 package lib
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 	"path/filepath"
-
-	"github.com/jh-bate/fantail-bot/Godeps/_workspace/src/github.com/jinzhu/configor"
 )
 
 func LoadConfig(config interface{}, name string) {
 
-	const config_path = "./config/"
+	const config_path = "config/"
+	const config_lib_path = "lib/config/"
 
-	absPath, err := filepath.Abs(config_path + name)
+	absPath, _ := filepath.Abs(config_path + name)
+	file, err := os.Open(absPath)
+
 	if err != nil {
 		log.Println("error getting config path", err.Error())
-		return
+		absPath, _ = filepath.Abs(config_lib_path + name)
+		file, err = os.Open(absPath)
 	}
-	configor.Load(&config, absPath)
+
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		log.Panic("error loading config ", err.Error())
+	}
+
 }
