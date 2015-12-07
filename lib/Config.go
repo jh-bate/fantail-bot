12bc9d636428e4f65c92584ab9b9c387385bf87a
Jamie <jamie@tidepool.org>
2015-12-07 14:55:16 +1300

    fallback path

--- lib/Config.go
+++ lib/Config.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"os"
 	"path/filepath"
 
 	"github.com/jh-bate/fantail-bot/Godeps/_workspace/src/github.com/jinzhu/configor"
@@ -10,5 +11,13 @@ func LoadConfig(config interface{}, name string) {
 
 	const config_path = "./config/"
 	absPath, _ := filepath.Abs(config_path + name)
+	_, err := os.Open(absPath)
+
+	if err != nil {
+
+		const config_lib_path = "./lib/config/"
+		absPath, _ = filepath.Abs(config_lib_path + name)
+	}
+
 	configor.Load(&config, absPath)
 }
