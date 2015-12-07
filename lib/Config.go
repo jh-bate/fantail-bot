72cc9e0e39f856c5ec1e2b58ab273c3bdc756207
Jamie <jamie@tidepool.org>
2015-12-07 13:42:28 +1300

    just basic path

--- lib/Config.go
+++ lib/Config.go
@@ -2,7 +2,6 @@ package lib
 
 import (
 	"log"
-	"os"
 	"path/filepath"
 
 	"github.com/jh-bate/fantail-bot/Godeps/_workspace/src/github.com/jinzhu/configor"
@@ -11,15 +10,9 @@ import (
 func LoadConfig(config interface{}, name string) {
 
 	const config_path = "./config/"
-	const lib_config_path = "./lib/config/"
 
 	absPath, _ := filepath.Abs(config_path + name)
-	_, err := os.Open(absPath)
-
-	if err != nil {
-		absPath, _ = filepath.Abs(lib_config_path + name)
-		log.Println("QandA path ", absPath)
-	}
+	log.Println("config path ", absPath)
 
 	configor.Load(&config, absPath)
 }
