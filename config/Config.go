c9963a1fa89c0b3c7521a0aaa8cc8f78b7056e48
Jamie <jamie@tidepool.org>
2015-12-16 23:03:24 +1300

    move config files

--- config/Config.go
+++ config/Config.go
@@ -11,12 +11,12 @@ func Load(config interface{}, name string) {
 
 	// we need to get the absolute path and also test it because of the
 	// difference of how this code runs in different environments
-	const config_path = "./_config/"
+	const config_path = "./config_files/"
 	absPath, _ := filepath.Abs(config_path + name)
 	_, err := os.Open(absPath)
 
 	if err != nil {
-		const config_lib_path = "./../_config/"
+		const config_lib_path = "./../config_files/"
 		absPath, _ = filepath.Abs(config_lib_path + name)
 	}
 
